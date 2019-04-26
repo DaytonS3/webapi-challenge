780a33e0081b01226239eedb4969da725bc4dec6
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-26 12:05:32 -0400

    added action post request

--- routes/actionRouter.js
+++ routes/actionRouter.js
@@ -15,4 +15,19 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const { project_id, description, notes } = req.body;
+  const newAction = { project_id, description, notes };
+  if (!project_id || !description || !notes) {
+    res.status(400).json({ error: "provide all fields" });
+  } else {
+    db.insert(newAction)
+      .then(action => {
+        res.json(action);
+      })
+      .catch(err => {
+        res.status(500).json({ error: "Error posing action" });
+      });
+  }
+});
 module.exports = router;
