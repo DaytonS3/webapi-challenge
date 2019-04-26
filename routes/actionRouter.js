68dc6cf06c08bfad0868e9798ed062a9f513101e
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-26 12:08:51 -0400

    added action get by id

--- routes/actionRouter.js
+++ routes/actionRouter.js
@@ -15,6 +15,23 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const actionId = req.params.id;
+  db.get(actionId)
+    .then(action => {
+      if (action) {
+        res.json(action);
+      } else {
+        res.status(404).json({ error: "No action with that ID" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error getting the action from database"
+      });
+    });
+});
+
 router.post("/", (req, res) => {
   const { project_id, description, notes } = req.body;
   const newAction = { project_id, description, notes };
